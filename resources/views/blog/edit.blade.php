ed89477ff24fa5522114d216ecedc7c9da90f76b
bana <bana.titech@gmail.com>
2019-10-12 16:26:42 +0900

    add category_split_space

---
+++ resources/views/blog/edit.blade.php
@@ -0,0 +1,36 @@
+@extends('base.base')
+
+@section('title', 'ばなてっく｜工業大学生のポートフォリオ')
+@section('description', '工業大学生によるプログラミングやガジェットに関するブログ兼Webアプリケーション置き場')
+@include('base.head')
+
+@section('content')
+<div class="uk-container uk-container-center uk-background-default">
+    <h1>記事編集</h1>
+    <form id="articleEdit" method="POST" action="/blog/edited/{{ $article->id }}" enctype="multipart/form-data">
+        <div class="form-group">
+            <label for="title">タイトル</label>
+            <input type="text" class="form-control" id="title" name="title" value="{{ article.title }}">
+        </div>
+        <div class="form-group">
+            <label for="category">カテゴリー</label>
+            <input type="text" class="form-control" id="category" name="category"
+                value="{{ article.category_split_space }}">
+        </div>
+        <div class="form-group">
+            <label for="content">内容</label>
+            <textarea class="form-control" id="content" name="content" rows="30">{{ $content }}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="img">画像</label>
+            <input type="file" class="form-control-file" id="image" name="image" multiple="multiple" accept="image/*">
+        </div>
+        <div class="form-group form-check">
+            <input type="radio" name="imgCheck" value="on">画像を変更する
+            <input type="radio" name="imgCheck" value="off" checked>画像を変更しない
+        </div>
+        {% csrf_token %}
+        <button type="submit" class="btn btn-primary">更新</button>
+    </form>
+</div>
+@endsection
--- routes/blog.php
+++ routes/blog.php
@@ -1,11 +1,12 @@
 <?php
+
 use Illuminate\Http\Request;
 use App\Article;
 use App\Category;
 
 Route::get('/blog', function () {
     $articles = Article::orderBy('created_at', 'asc')->get();
-    
+
     return view('blog.blog', [
         'articles' => $articles
     ]);
@@ -13,9 +14,9 @@ Route::get('/blog', function () {
 
 Route::get('/blog/{articleId}', function ($articleId) {
     $articleCount = \App\Article::where('id', $articleId)->count();
-    if($articleCount == 0){
+    if ($articleCount == 0) {
         return App::abort(404);
-    }else{
+    } else {
         $article = \App\Article::where('id', $articleId)->first();
         return view('blog.view', [
             'article' => $article
@@ -25,10 +26,20 @@ Route::get('/blog/{articleId}', function ($articleId) {
 
 Route::get('/blog/delete/{articleId}', function ($articleId) {
     $article = \App\Article::where('id', $articleId)->first();
-    $deleteDirPath = "item/article/".$article->id;
+    $deleteDirPath = "item/article/" . $article->id;
     File::deleteDirectory(public_path($deleteDirPath));
     $article->delete();
     return redirect('/blog');
+});
+
+Route::get('/blog/edit/{articleId}', function ($articleId) {
+    $article = \App\Article::where('id', $articleId)->first();
+    $editPath = public_path(("item/" . $article->md_file));
+    $content = file_get_contents($editPath);
+    return view('blog.edit', [
+        'article' => $article,
+        'content' => $content
+    ]);
 });
 
 Route::get('/blog/post', function () {
@@ -39,7 +50,7 @@ Route::post('/blog/posted', function (Request $request) {
     $validator = Validator::make($request->all(), [
         'title' => 'required|max:255',
         'category' => 'required|max:255',
-        'mdfile'=>'required',
+        'mdfile' => 'required',
     ]);
 
     if ($validator->fails()) {
@@ -49,36 +60,37 @@ Route::post('/blog/posted', function (Request $request) {
     }
     $article = new Article();
     $article->title = $request->title;
+    $article->category_split_space = $request->category;
     $article->md_file = "dummy";
     $article->save();
     $mdFile = $request->file('mdfile');
-    $mdFilePath = $mdFile->storeAs('article/'.$article->id,'article.md');
+    $mdFilePath = $mdFile->storeAs('article/' . $article->id, 'article.md');
     $article->md_file = $mdFilePath;
     $article->save();
-    foreach($request->file('img') as $img){
-        $imgName = $img->getClientOriginalName();
-        $img->storeAs('article/'.$article->id.'/image',$imgName);
+    if ($request->file('img') != null) {
+        foreach ($request->file('img') as $img) {
+            $imgName = $img->getClientOriginalName();
+            $img->storeAs('article/' . $article->id . '/image', $imgName);
+        }
     }
 
     $categorySplitSpace = explode(" ", $request->category);
-    foreach($categorySplitSpace as $category){
+    foreach ($categorySplitSpace as $category) {
         $categoryCount = \App\Category::where('name', $category)->count();
-        if($categoryCount == 0){
+        if ($categoryCount == 0) {
             $newCategory = new Category();
             $newCategory->name = $category;
             $newCategory->save();
             $article->categories()->attach($newCategory->id);
-        }else{
+        } else {
             $categoryInDB = \App\Category::where('name', $category)->first();
             $article->categories()->attach($categoryInDB->id);
         }
     }
 
     $articles = Article::orderBy('created_at', 'asc')->get();
-    
+
     return view('blog.blog', [
         'articles' => $articles
     ]);
-    
 });
-
