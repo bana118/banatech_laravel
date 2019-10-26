ae687b5111192a34c60ee189cba18971a799d695
bana <bana.titech@gmail.com>
2019-10-26 18:27:34 +0900

    add autholization to edit, delete and post blog

--- routes/blog.php
+++ routes/blog.php
@@ -23,8 +23,8 @@ Route::get('/blog/view/{articleId}', function ($articleId) {
         $categories = $article->categories()->get();
         foreach ($categories as $category) {
             $categoryArticles = $category->articles()->get();
-            foreach($categoryArticles as $categoryArticle){
-                if(!in_array($categoryArticle->id, $relatedArticleIdList) && $categoryArticle->id != $articleId){
+            foreach ($categoryArticles as $categoryArticle) {
+                if (!in_array($categoryArticle->id, $relatedArticleIdList) && $categoryArticle->id != $articleId) {
                     $relatedArticleIdList[] = $categoryArticle->id;
                 }
             }
@@ -38,124 +38,139 @@ Route::get('/blog/view/{articleId}', function ($articleId) {
 });
 
 Route::get('/blog/delete/{articleId}', function ($articleId) {
-    $article = \App\Article::where('id', $articleId)->first();
-    $deleteDirPath = "item/article/" . $article->id;
-    File::deleteDirectory(public_path($deleteDirPath));
-    $article->delete();
-    return redirect('/blog');
+    if (Auth::check()) {
+        $article = \App\Article::where('id', $articleId)->first();
+        $deleteDirPath = "item/article/" . $article->id;
+        File::deleteDirectory(public_path($deleteDirPath));
+        $article->delete();
+        return redirect('/blog');
+    } else {
+        return redirect('/admin/login');
+    }
 });
 
 Route::get('/blog/edit/{articleId}', function ($articleId) {
-    $article = \App\Article::where('id', $articleId)->first();
-    $editPath = public_path(("item/" . $article->md_file));
-    $content = file_get_contents($editPath);
-    return view('blog.edit', [
-        'article' => $article,
-        'content' => $content
-    ]);
+    if (Auth::check()) {
+        $article = \App\Article::where('id', $articleId)->first();
+        $editPath = public_path(("item/" . $article->md_file));
+        $content = file_get_contents($editPath);
+        return view('blog.edit', [
+            'article' => $article,
+            'content' => $content
+        ]);
+    } else {
+        return redirect('/admin/login');
+    }
 });
 
 Route::post('/blog/edited/{articleId}', function (Request $request, $articleId) {
-    $validator = Validator::make($request->all(), [
-        'title' => 'required|max:255',
-        'category' => 'required|max:255',
-    ]);
-    if ($validator->fails()) {
-        return redirect('/blog/edit/' . $articleId)
-            ->withInput()
-            ->withErrors($validator);
-    }
-
-    $article = \App\Article::where('id', $articleId)->first();
-    $article->title = $request->title;
-    $article->category_split_space = $request->category;
-    $article->categories()->detach();
-    $categorySplitSpace = explode(" ", $request->category);
-    foreach ($categorySplitSpace as $category) {
-        $categoryCount = \App\Category::where('name', $category)->count();
-        if ($categoryCount == 0) {
-            $newCategory = new Category();
-            $newCategory->name = $category;
-            $newCategory->save();
-            $article->categories()->attach($newCategory->id);
-        } else {
-            $categoryInDB = \App\Category::where('name', $category)->first();
-            $article->categories()->attach($categoryInDB->id);
+    if (Auth::check()) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'category' => 'required|max:255',
+        ]);
+        if ($validator->fails()) {
+            return redirect('/blog/edit/' . $articleId)
+                ->withInput()
+                ->withErrors($validator);
         }
+
+        $article = \App\Article::where('id', $articleId)->first();
+        $article->title = $request->title;
+        $article->category_split_space = $request->category;
+        $article->categories()->detach();
+        $categorySplitSpace = explode(" ", $request->category);
+        foreach ($categorySplitSpace as $category) {
+            $categoryCount = \App\Category::where('name', $category)->count();
+            if ($categoryCount == 0) {
+                $newCategory = new Category();
+                $newCategory->name = $category;
+                $newCategory->save();
+                $article->categories()->attach($newCategory->id);
+            } else {
+                $categoryInDB = \App\Category::where('name', $category)->first();
+                $article->categories()->attach($categoryInDB->id);
+            }
+        }
+
+        $content = $request->content;
+        $mdFilePath = public_path('item/article/' . $article->id . '/article.md');
+        \File::put($mdFilePath, $content);
+
+        $imgCheck = $request->imgCheck;
+        if ($imgCheck == "on") {
+            $imgDir = public_path('item/article/' . $article->id . '/image');
+            \File::cleanDirectory($imgDir);
+            if ($request->file('img') != null) {
+                foreach ($request->file('img') as $img) {
+                    $imgName = $img->getClientOriginalName();
+                    $img->storeAs('article/' . $article->id . '/image', $imgName);
+                }
+            }
+        }
+
+        $article->save();
+
+        return redirect('/blog');
+    } else {
+        return redirect('/admin/login');
     }
+});
 
-    $content = $request->content;
-    $mdFilePath = public_path('item/article/' . $article->id . '/article.md');
-    \File::put($mdFilePath, $content);
+Route::get('/blog/post', function () {
+    if (Auth::check()) {
+        return view('blog.post');
+    } else {
+        return redirect('/admin/login');
+    }
+});
 
-    $imgCheck = $request->imgCheck;
-    if ($imgCheck == "on") {
-        $imgDir = public_path('item/article/' . $article->id . '/image');
-        \File::cleanDirectory($imgDir);
+Route::post('/blog/posted', function (Request $request) {
+    if (Auth::check()) {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|max:255',
+            'category' => 'required|max:255',
+            'mdfile' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect('/blog/post')
+                ->withInput()
+                ->withErrors($validator);
+        }
+        $article = new Article();
+        $article->title = $request->title;
+        $article->category_split_space = $request->category;
+        $article->md_file = "dummy";
+        $article->save();
+        $mdFile = $request->file('mdfile');
+        $mdFilePath = $mdFile->storeAs('article/' . $article->id, 'article.md');
+        $article->md_file = $mdFilePath;
+        $article->save();
         if ($request->file('img') != null) {
             foreach ($request->file('img') as $img) {
                 $imgName = $img->getClientOriginalName();
                 $img->storeAs('article/' . $article->id . '/image', $imgName);
             }
         }
-    }
 
-    $article->save();
-
-    return redirect('/blog');
-});
-
-Route::get('/blog/post', function () {
-    return view('blog.post');
-});
-
-Route::post('/blog/posted', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'title' => 'required|max:255',
-        'category' => 'required|max:255',
-        'mdfile' => 'required',
-    ]);
-
-    if ($validator->fails()) {
-        return redirect('/blog/post')
-            ->withInput()
-            ->withErrors($validator);
-    }
-    $article = new Article();
-    $article->title = $request->title;
-    $article->category_split_space = $request->category;
-    $article->md_file = "dummy";
-    $article->save();
-    $mdFile = $request->file('mdfile');
-    $mdFilePath = $mdFile->storeAs('article/' . $article->id, 'article.md');
-    $article->md_file = $mdFilePath;
-    $article->save();
-    if ($request->file('img') != null) {
-        foreach ($request->file('img') as $img) {
-            $imgName = $img->getClientOriginalName();
-            $img->storeAs('article/' . $article->id . '/image', $imgName);
+        $categorySplitSpace = explode(" ", $request->category);
+        foreach ($categorySplitSpace as $category) {
+            $categoryCount = \App\Category::where('name', $category)->count();
+            if ($categoryCount == 0) {
+                $newCategory = new Category();
+                $newCategory->name = $category;
+                $newCategory->save();
+                $article->categories()->attach($newCategory->id);
+            } else {
+                $categoryInDB = \App\Category::where('name', $category)->first();
+                $article->categories()->attach($categoryInDB->id);
+            }
         }
+        return redirect('/blog');
+    } else {
+        return redirect('/admin/login');
     }
-
-    $categorySplitSpace = explode(" ", $request->category);
-    foreach ($categorySplitSpace as $category) {
-        $categoryCount = \App\Category::where('name', $category)->count();
-        if ($categoryCount == 0) {
-            $newCategory = new Category();
-            $newCategory->name = $category;
-            $newCategory->save();
-            $article->categories()->attach($newCategory->id);
-        } else {
-            $categoryInDB = \App\Category::where('name', $category)->first();
-            $article->categories()->attach($categoryInDB->id);
-        }
-    }
-
-    $articles = Article::orderBy('created_at', 'asc')->get();
-
-    return view('blog.blog', [
-        'articles' => $articles
-    ]);
 });
 
 Route::post('/blog/search', function (Request $request) {
