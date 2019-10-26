229af0f501eed7d4e3af2d622cdd5efb59e7c34f
bana <bana.titech@gmail.com>
2019-10-26 17:06:32 +0900

    add voyager admin page

--- routes/home.php
+++ routes/home.php
@@ -8,7 +8,12 @@ Route::get('privacy_policy', function () {
     return view('home.privacy_policy');
 });
 
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
 
+/*
 Route::get('/sample', function () { //TODO for debug
     return view('home.sample');
 });
+*/
