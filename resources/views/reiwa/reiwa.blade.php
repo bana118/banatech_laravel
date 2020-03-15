250fbb32756ac3f38c5e2a4cb6fc11e4c098bc84
bana <bana.titech@gmail.com>
2020-03-16 00:14:13 +0900

    set static files for reiwa app

---
+++ resources/views/reiwa/reiwa.blade.php
@@ -0,0 +1,28 @@
+@extends('base.base')
+
+@section('title', '令和で書き初め')
+@section('description', '「令和」を100秒以内に書きまくれ！手書き文字を人工知能が判定')
+@include('base.head')
+
+@section('content')
+<div class="uk-container" style="touch-action: manipulation;">
+    <div class="uk-frex start" style="position: relative;text-align:center;">
+        <img src="{{ asset('/img/reiwa/write1.png') }}" onClick="start();">
+        <div id="bgroup" class="uk-button-group" style="display:none; position:absolute;top: 0;left: 0;right: 0;bottom: 0;margin: auto;width:100px;height:100px;">
+            <button type="button" class="uk-button uk-button-primary" onclick="location.href='reiwa/solo'">Play</button>
+        </div>
+        <h4>画像をタッチorクリック！</h4>
+    </div>
+    <h1>令和で書き初め</h1>
+    <p>ルール：100秒以内に「令和」を書きまくれ！マウスか指で書いた文字を人工知能が判定！</p>
+    <p>注意：IE, Edgeブラウザでは動作しません。Google Chrome, Opera, Firefox, sleipnirなどを推奨します。「令」の字は2つありますがどちらでも大丈夫です。</p>
+    <p>効果音：<a href="https://maoudamashii.jokersounds.com/">魔王魂</a>様</p>
+    <p>BGM：<a href="http://www.rengoku-teien.com/index.html">煉獄庭園</a>様</p>
+</div>
+<script>
+    function start(){
+        var bgroup = document.getElementById("bgroup");
+        bgroup.style.display="inline-block";
+    }
+</script>
+@endsection
