c33a184465668653646d7489b066f0cfad794ff9
bana <bana.titech@gmail.com>
2020-03-24 19:59:12 +0900

    add favicon

--- resources/views/base/head.blade.php
+++ resources/views/base/head.blade.php
@@ -3,6 +3,12 @@
 <meta name="author" content="bana">
 <meta name="theme-color" content="#0033FF">
 <meta name="description" content="@yield('description')">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#0033FF">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img/favicon/apple-touch-icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('img/favicon/favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('img/favicon/favicon-16x16.png') }}">
+<link rel="mask-icon" href="{{ asset('img/favicon/safari-pinned-tab.svg') }}" color="#5bbad5">
 <title>
     @yield('title')
 </title>
