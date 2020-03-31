c04b9f8770c262507fe841a8eed9a2b317b2c322
bana <bana.titech@gmail.com>
2020-04-01 08:32:14 +0900

    inplement create maze method

--- resources/js/vr_meiro/play.js
+++ resources/js/vr_meiro/play.js
@@ -18,16 +18,75 @@ function createMaze(size) {
     let startCreateWallPoints = [];
     for (let i = 2; i < size - 1; i += 2) {
         for (let j = 2; j < size - 1; j += 2) {
-            startCreateWallPoints.append([[i, j]]);
+            startCreateWallPoints.push([i, j]);
         }
     }
-
-    while(startCreateWallPoints.length != 0){
-        let randIndex = Math.floor(Math.random()*startCreateWallPoints.length); //0 ~ startCreateWallPoints.length - 1
+    while (startCreateWallPoints.length != 0) {
+        let randIndex = Math.floor(Math.random() * startCreateWallPoints.length); //0 ~ startCreateWallPoints.length - 1
         let startCreateWallPoint = startCreateWallPoints.pop(randIndex);
-        if(BINARY_ARRAY[startCreateWallPoint[0]][startCreateWallPoint[1]] == 0){
+        if (BINARY_ARRAY[startCreateWallPoint[0]][startCreateWallPoint[1]] == 0) {
+            let currentCreatingWallEvenPoints = [];
             BINARY_ARRAY[startCreateWallPoint[0]][startCreateWallPoint[1]] = 1;
-            let currentCreatingWall = [startCreateWallPoint];
+            currentCreatingWallEvenPoints.push(startCreateWallPoint);
+            let canExtendWall = true;
+            let x = startCreateWallPoint[0];
+            let y = startCreateWallPoint[1];
+            while (canExtendWall) {
+                let extendDirection = [];
+                if (BINARY_ARRAY[x - 1][y] == 0 && !currentCreatingWallEvenPoints.includes([x - 2, y])) {
+                    extendDirection.push("left");
+                }
+                if (BINARY_ARRAY[x + 1][y] == 0 && !currentCreatingWallEvenPoints.includes([x + 2, y])) {
+                    extendDirection.push("right");
+                }
+                if (BINARY_ARRAY[x][y - 1] == 0 && !currentCreatingWallEvenPoints.includes([x, y - 2])) {
+                    extendDirection.push("down");
+                }
+                if (BINARY_ARRAY[x][y + 1] == 0 && !currentCreatingWallEvenPoints.includes([x, y + 2])) {
+                    extendDirection.push("up");
+                }
+                if (extendDirection.length != 0) {
+                    let randomDirectionIndex = Math.floor(Math.random() * extendDirection.length);
+                    let randomDirection = extendDirection[randomDirectionIndex];
+                    if (randomDirection == "left") {
+                        canExtendWall = (BINARY_ARRAY[x - 2][y] == 0);
+                        BINARY_ARRAY[x - 1][y] = 1;
+                        BINARY_ARRAY[x - 2][y] = 1;
+                        currentCreatingWallEvenPoints.push([x - 2, y]);
+                        if (canExtendWall) {
+                            x = x - 2;
+                        }
+                    } else if (randomDirection == "right") {
+                        canExtendWall = (BINARY_ARRAY[x + 2][y] == 0);
+                        BINARY_ARRAY[x + 1][y] = 1;
+                        BINARY_ARRAY[x + 2][y] = 1;
+                        currentCreatingWallEvenPoints.push([x + 2, y]);
+                        if (canExtendWall) {
+                            x = x + 2;
+                        }
+                    } else if (randomDirection == "down") {
+                        canExtendWall = (BINARY_ARRAY[x][y - 2] == 0);
+                        BINARY_ARRAY[x][y - 1] = 1;
+                        BINARY_ARRAY[x][y - 2] = 1;
+                        currentCreatingWallEvenPoints.push([x, y - 2]);
+                        if (canExtendWall) {
+                            y = y - 2;
+                        }
+                    } else { //randomDirection == "up"
+                        canExtendWall = (BINARY_ARRAY[x][y + 2] == 0);
+                        BINARY_ARRAY[x][y + 1] = 1;
+                        BINARY_ARRAY[x][y + 2] = 1;
+                        currentCreatingWallEvenPoints.push([x, y + 2]);
+                        if (canExtendWall) {
+                            y = y + 2;
+                        }
+                    }
+                } else {
+                    let previousPoint = currentCreatingWallEvenPoints.pop();
+                    x = previousPoint[0];
+                    y = previousPoint[1];
+                }
+            }
         }
     }
     return BINARY_ARRAY;
