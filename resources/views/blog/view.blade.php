fbe3be323e6ed8f9ddf32809473381d20407b5d2
bana <bana.titech@gmail.com>
2020-06-29 10:15:19 +0900

    show title html

--- resources/views/blog/view.blade.php
+++ resources/views/blog/view.blade.php
@@ -1,7 +1,8 @@
 @extends('base.base')
 
-@section('title', '{{ $article->title }}')
-@section('description', '工業大学生によるプログラミングやガジェットに関するブログ兼Webアプリケーション置き場')
+@section('title')
+{{ $article->title }}
+@endsection
 @section('head')
 @include('base.head')
 <script src="{{ mix('js/blog/view.js') }}"></script>
