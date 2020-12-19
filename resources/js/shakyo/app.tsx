aa111559c9dbab8bcbe30ac2f79c010cfb99c6f5
bana118 <bana.titech@gmail.com>
2020-12-19 16:56:39 +0900

    init typescript

--- resources/js/shakyo/app.tsx
+++ resources/js/shakyo/app.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-    return <h1>Hello React!</h1>;
-}
+const App: React.FC = () => {
+    return <div>Hello World</div>;
+};
 
-const hoge = document.getElementById("app");
-console.log(hoge);
 if (document.getElementById("app")) {
-    console.log("hoge");
     ReactDOM.render(<App />, document.getElementById("app"));
 }
--- webpack.mix.js
+++ webpack.mix.js
@@ -55,7 +55,7 @@ mix.js("resources/js/vr_meiro/intro.js", "public/js/vr_meiro/").js(
 );
 
 // shakyo
-mix.react("resources/js/shakyo/app.js", "public/js/shakyo");
+mix.ts("resources/js/shakyo/app.tsx", "public/js/shakyo");
 
 if (mix.inProduction()) {
     mix.version();
