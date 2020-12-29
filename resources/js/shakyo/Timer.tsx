f913e3975e74441ef0452b28ac6ada67369f0ab2
bana118 <bana.titech@gmail.com>
2020-12-29 22:23:36 +0900

    recort all taken times and save it as csv

--- resources/js/shakyo/Timer.tsx
+++ resources/js/shakyo/Timer.tsx
@@ -1,55 +1,77 @@
 import React, { ReactElement, useState, useRef, useEffect } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 
 interface TimerProps {
-    milliSeconds: number;
-    updateMilliSeconds: (milliSeconds: number) => void;
-    milliInterval: number;
     count: number;
 }
 
 export const Timer = (props: TimerProps): ReactElement => {
+    const anchorRef = useRef<HTMLAnchorElement | null>(null);
+    useHotkeys("ctrl+a", (event: KeyboardEvent) => {
+        startTimer();
+        event.preventDefault();
+    });
     const [isStarted, setIsStarted] = useState(false);
     const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout | undefined>(
         undefined
     );
+    const [milliSeconds, setMilliSeconds] = useState(0);
+    const milliInterval = 50;
     const [milliSecondsArray, setMilliSecondsArray] = useState<number[]>([]);
 
     useEffect(() => {
         if (isStarted) {
-            const id = setTimeout(countTime, props.milliInterval);
+            const id = setTimeout(countTime, milliInterval);
             setTimeoutId(id);
             return () => {
                 clearTimeout(id);
             };
         }
-    }, [props.milliSeconds, isStarted]);
+    }, [milliSeconds, isStarted]);
 
     useEffect(() => {
         if (timeoutId != null) {
             clearTimeout(timeoutId);
-            props.updateMilliSeconds(0);
-            setMilliSecondsArray([...milliSecondsArray, props.milliSeconds]);
+            setMilliSeconds(0);
+            setMilliSecondsArray([...milliSecondsArray, milliSeconds]);
         }
     }, [props.count]);
 
     const countTime = () => {
-        props.updateMilliSeconds(props.milliSeconds + props.milliInterval);
+        setMilliSeconds(milliSeconds + milliInterval);
     };
     const startTimer = () => {
         setIsStarted(true);
     };
-    const stopTimer = () => {
+    const finish = () => {
         if (timeoutId != null) {
             clearTimeout(timeoutId);
+        }
+        const anchor = anchorRef.current;
+        if (anchor == null) {
+            console.log("anchor loading error");
+        } else {
+            const header = "id,time";
+            const content = milliSecondsArray.map((ms, i) => `${i + 1},${ms}`);
+            const csv = [header, ...content].join("\n");
+            const bom = new Uint8Array([0xef, 0xbb, 0xbf]);
+            const blob = new Blob([bom, csv], { type: "text/csv" });
+            const url = window.URL.createObjectURL(blob);
+            anchor.download = "times.csv";
+            anchor.href = url;
+            // cause network error
+            // window.URL.revokeObjectURL(url);
         }
     };
 
     return (
         <React.Fragment>
-            <p>{props.milliSeconds}ms</p>
+            <p>{milliSeconds}ms</p>
             <p>{milliSecondsArray}</p>
-            <button onClick={startTimer}>開始</button>
-            <button onClick={stopTimer}>停止</button>
+            <a onClick={startTimer}>開始</a>
+            <a ref={anchorRef} onClick={finish}>
+                終了
+            </a>
         </React.Fragment>
     );
 };
