68d975b585cd449279212d4ed65c03291a70b4d2
bana118 <bana.titech@gmail.com>
2020-12-29 17:33:16 +0900

    add hot key(Ctrl+s) to download and clear when downloading

--- resources/js/shakyo/DownloadLink.tsx
+++ resources/js/shakyo/DownloadLink.tsx
@@ -1,4 +1,5 @@
 import React, { ReactElement, useState, useRef, useEffect } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
 
 interface DownloadButtonProps {
     canvas: HTMLCanvasElement | undefined;
@@ -8,6 +9,14 @@ interface DownloadButtonProps {
 export const DownloadLink = (props: DownloadButtonProps): ReactElement => {
     const anchorRef = useRef<HTMLAnchorElement | null>(null);
     const [count, setCount] = useState(1);
+    useHotkeys("ctrl+s", (event: KeyboardEvent) => {
+        const anchor = anchorRef.current;
+        if (anchor != null) {
+            downloadCanvas();
+            anchor.click();
+        }
+        event.preventDefault();
+    });
     useEffect(() => {
         props.updateCount(count);
     }, [count]);
@@ -19,12 +28,28 @@ export const DownloadLink = (props: DownloadButtonProps): ReactElement => {
                 anchor.download = `${count}.png`;
                 setCount(count + 1);
                 anchor.href = base64;
+                const context = props.canvas.getContext("2d");
+                if (context != null) {
+                    context.fillStyle = "rgb(255,255,255)";
+                    context.fillRect(
+                        0,
+                        0,
+                        context.canvas.width,
+                        context.canvas.height
+                    );
+                } else {
+                    console.log("context loading error");
+                }
+            } else {
+                console.log("canvas loading error");
             }
+        } else {
+            console.log("anchor loading error");
         }
     };
     return (
         <a ref={anchorRef} onClick={downloadCanvas}>
-            保存
+            保存(Ctrl+s)
         </a>
     );
 };
