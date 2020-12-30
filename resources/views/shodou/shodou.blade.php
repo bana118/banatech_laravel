f7feffc8ba0643eb8b290919bc5f250bfb8820dc
bana118 <bana.titech@gmail.com>
2020-12-30 18:30:09 +0900

    modify description

--- resources/views/shodou/shodou.blade.php
+++ resources/views/shodou/shodou.blade.php
@@ -1,15 +1,15 @@
 @extends('base.base')
 
 @section('title', 'デジタル書道(手書き文字認識用のデータセット作成)')
-@section('description', '手書き文字認識のデータセット作成用デジタル書道アプリ．手書き文字認識用の手書き文字をタッチやマウスでガンガン書いて保存できます．')
+@section('description', '手書き文字認識のデータセット作成用デジタル書道アプリ．キャンバスに文字をタッチやマウスで書いて保存できます．')
 @section('head')
     @include('base.head')
     <script src="{{ mix('js/shodou/App.js') }}" defer></script>
     <meta property="og:type" content="article" />
     <meta property="og:url" content="{{ url()->current() }}" />
     <meta property="og:locale" content="ja_JP">
-    <meta property="og:title" content="ばなてっく" />
-    <meta property="og:description" content="デジタル書道(手書き文字認識用のデータセット作成)" />
+    <meta property="og:title" content="デジタル書道(手書き文字認識用のデータセット作成)'" />
+    <meta property="og:description" content="手書き文字認識のデータセット作成用デジタル書道アプリ．キャンバスに文字をタッチやマウスで書いて保存できます．" />
     <meta property="og:image" content="{{ asset('img/favicon/android-chrome-512x512.png') }}" />
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:site" content="{{ Config::get('const.TWITTERID') }}" />
