a90943054fb97a214b45bc407e3d04342da1d796
bana118 <bana.titech@gmail.com>
2021-01-01 00:45:18 +0900

    add generate html articles scripts

--- resources/js/blog/view.js
+++ resources/js/blog/view.js
@@ -1,7 +1,5 @@
 /* eslint no-import-assign: 0 */
 /* eslint no-undef: 0 */
-import marked from "marked";
-import hljs from "highlightjs";
 import MathJax from "mathjax";
 
 MathJax = {
@@ -32,71 +30,5 @@ MathJax = {
     },
 };
 window.onload = function () {
-    const renderer = new marked.Renderer();
-    renderer.code = function (code, language) {
-        if (language.indexOf(":") != -1) {
-            const lang = language.split(":")[0];
-            const fileName = language.split(":")[1].trim();
-            return (
-                "<pre>" +
-                '<div class="uk-badge" style="display: inline-block;">' +
-                fileName +
-                "</div>" +
-                '<code class="hljs">' +
-                hljs.highlightAuto(code, [lang]).value +
-                "</code></pre>"
-            );
-        } else {
-            return (
-                "<pre" +
-                '><code class="hljs">' +
-                hljs.highlightAuto(code).value +
-                "</code></pre>"
-            );
-        }
-    };
-    renderer.image = function (href, title, text) {
-        const fileName = href.split("/").pop();
-        const articleIdElement = document.getElementById("article_id");
-        const articleId = articleIdElement.dataset.name;
-        const imgPath =
-            location.origin + "/uploaded/article/" + articleId + "/image";
-        return (
-            '<img src="' +
-            imgPath +
-            "/" +
-            fileName +
-            '" alt="' +
-            text +
-            '" class="img-fluid">'
-        );
-    };
-    marked.setOptions({
-        renderer: renderer,
-        gfm: true,
-        tables: true,
-        breaks: true,
-        pedantic: false,
-        sanitize: false,
-        smartLists: false,
-        smartypants: false,
-    });
-
-    const target = document.getElementById("markdownContent");
-    const url = target.getAttribute("src");
-    fetch(url)
-        .then((response) => {
-            if (!response.ok) {
-                target.append("This content failed to load.");
-                throw new Error("Network response was not ok");
-            }
-            return response.text();
-        })
-        .then((data) => {
-            target.insertAdjacentHTML("beforeend", marked(data));
-            MathJax.typeset();
-        })
-        .catch(() => {
-            target.append("This content failed to load.");
-        });
+    MathJax.typeset();
 };
--- scripts/generate_articles.js
+++
@@ -1,2 +0,0 @@
-// generate HTML article from markdown
-console.log("hoge");
