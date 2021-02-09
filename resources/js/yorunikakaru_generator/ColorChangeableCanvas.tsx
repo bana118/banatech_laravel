39a61bbddb9be4211764f7dd7e8362e2c704e944
bana118 <bana.titech@gmail.com>
2021-02-09 15:43:17 +0900

    modify canvas size

--- resources/js/yorunikakaru_generator/ColorChangeableCanvas.tsx
+++ resources/js/yorunikakaru_generator/ColorChangeableCanvas.tsx
@@ -24,6 +24,8 @@ export const ColorChangeableCanvas = (
         image.onload = () => {
             if (canvas != null) {
                 const context = canvas.getContext("2d");
+                canvas.width = image.width;
+                canvas.height = image.height;
                 context?.drawImage(image, 0, 0);
             }
         };
