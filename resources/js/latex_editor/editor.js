d3f8b91b84b394d1bd31ad3201066347ea13fbd1
bana118 <bana.titech@gmail.com>
2021-07-09 13:48:04 +0900

    remove jquery

--- resources/js/latex_editor/editor.js
+++ resources/js/latex_editor/editor.js
@@ -2,10 +2,8 @@ import "ace-builds";
 import "ace-builds/webpack-resolver";
 import { parse, HtmlGenerator } from "latex.js";
 
-window.$ = window.jQuery = require("jquery");
-
 var editor;
-window.onload = function() {
+window.onload = function () {
     editor = ace.edit("editor", {
         mode: "ace/mode/latex",
         theme: "ace/theme/monokai",
@@ -13,42 +11,40 @@ window.onload = function() {
     });
     editor.getSession().setUseWrapMode(true);
     editor.getSession().setTabSize(4);
-    editor.$blockScrolling = Infinity;
-    editor.getSession().on("change", function() {
+    editor.getSession().on("change", function () {
         var generator = new HtmlGenerator({
             hyphenate: false,
         });
+        const outputEl = document.getElementById("output");
         try {
             generator = new parse(editor.getValue(), {
                 generator: generator,
             });
-            $("#output").empty();
-            output.appendChild(
+            outputEl.textContent = "";
+            outputEl.appendChild(
                 generator.stylesAndScripts(
                     "https://cdn.jsdelivr.net/npm/latex.js@0.12.1/dist/"
                 )
             );
-            output.appendChild(generator.domFragment());
+            outputEl.appendChild(generator.domFragment());
         } catch (e) {
             if (e.name == "SyntaxError") {
                 /*LaTeXのパースエラー*/
-                $("#output").replaceWith(
+                outputEl.outerHTML =
                     '<div id="output"> <p>' +
-                        e.name +
-                        "</p><p>line " +
-                        e.location["start"]["line"] +
-                        " (column " +
-                        e.location["start"]["column"] +
-                        "): " +
-                        e.message +
-                        "</p></div>"
-                );
+                    e.name +
+                    "</p><p>line " +
+                    e.location["start"]["line"] +
+                    " (column " +
+                    e.location["start"]["column"] +
+                    "): " +
+                    e.message +
+                    "</p></div>";
                 console.error(e);
             } else {
                 /*予期せぬエラー*/
-                $("#output").replaceWith(
-                    '<div id="output"> <p>unexpected error' + "</p></div>"
-                );
+                outputEl.outerHTML =
+                    '<div id="output"> <p>unexpected error' + "</p></div>";
                 console.error(e);
             }
         }
@@ -56,28 +52,26 @@ window.onload = function() {
 };
 
 function postForm(url, data) {
-    var $form = $("<form/>", {
-        action: url,
-        method: "post",
-        id: "tempForm",
-    });
-    for (var key in data) {
-        $form.append(
-            $("<input/>", {
-                type: "hidden",
-                name: key,
-                value: data[key],
-            })
-        );
+    const formEl = document.createElement("form");
+    formEl.setAttribute("method", "post");
+    formEl.setAttribute("action", url);
+    for (const key in data) {
+        const inputEl = document.createElement("input");
+        inputEl.setAttribute("type", "hidden");
+        inputEl.setAttribute("name", key);
+        inputEl.setAttribute("value", data[key]);
+        formEl.appendChild(inputEl);
     }
-    var csrfToken = $('meta[name="csrf-token"]').attr("content");
-    $form.append(csrfToken);
-    $form.appendTo(document.body);
-    $form.submit();
-    $("#tempForm").remove();
+    const csrfToken = document
+        .querySelector("meta[name=csrf-token]")
+        .getAttribute("content");
+    formEl.appendChild(csrfToken);
+    document.body.appendChild(formEl);
+    formEl.submit();
+    formEl.remove();
 }
 
-window.saveTex = function() {
+window.saveTex = function () {
     var tex = editor.getValue();
     postForm("/latex_editor/save", {
         tex: tex,
@@ -92,10 +86,10 @@ window.saveTex = function() {
     });
 }*/
 
-window.undo = function() {
+window.undo = function () {
     editor.undo();
 };
 
-window.redo = function() {
+window.redo = function () {
     editor.redo();
 };
